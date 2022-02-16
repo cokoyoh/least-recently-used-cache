dae6c270fbe330a496ace21083c4fc2cc1a4e4fc
cokoyoh <charlesokoyoh@gmail.com>
2022-02-16 13:28:46 +0300

    Finalise LRUcache collection

---
+++ src/collection.ts
@@ -0,0 +1,52 @@
+interface LRUCacheInterface<K, V> {
+  get(key: K): V | number;
+  put(key: K, value: V): void
+}
+
+class Collection<K, V> implements LRUCacheInterface<K, V> {
+  private capacity: number;
+  private collection: Map<K, V>;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+    this.collection = new Map<K, V>();
+  }
+
+  private update(key: K, value: V): void {
+    this.collection.delete(key);
+    this.collection.set(key, value);
+  }
+
+  get(key: K): V | number {
+    if (!this.collection.has(key)) {
+      return -1;
+    }
+
+    const value: V = this.collection.get(key);
+
+    this.update(key, value);
+
+    return value;
+  }
+
+  put(key: K, value: V) {
+    const keyExists = this.collection.has(key);
+
+    if (keyExists) {
+      this.update(key, value);
+    }
+
+    this.collection.set(key, value);
+
+    if (this.collection.size > this.capacity) {
+      const leastUsedKey = this.collection.keys().next().value;
+      this.collection.delete(leastUsedKey);
+    }
+  }
+
+  show(): void {
+    console.log({collection: this.collection });
+  }
+}
+
+export { Collection };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const username: string = 'Jane Doe';
+import { Collection } from './collection';
 
-console.log({ username });
+const lRUCache = new Collection<number, number>(2);
+lRUCache.put(1, 1);
+lRUCache.put(2, 2);
+lRUCache.get(1);
+lRUCache.put(3, 3);
+// const keyTwo = lRUCache.get(2);
+// console.log({ keyTwo });
+lRUCache.put(4, 4);
+console.log(lRUCache.get(1))
+console.log(lRUCache.get(3))
+console.log(lRUCache.get(4))
+console.log(lRUCache.show());
